e75b101898c4c34b3e40ceed116ebf130bf8f741
Anish-1144 <dev.anishmourya27@gmail.com>
2025-03-22 17:55:35 +0530

    ...

--- src/pages/Contect.jsx
+++ src/pages/Contect.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Footer from '../components/footer/Footer'
 import Nevbar from '../components/header/Nevbar'
-import Contact from '../components/contact/Contact'
+import Contact from '../components/Contact/Contact'
 
 function Contect() {
   return (
